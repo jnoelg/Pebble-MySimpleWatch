57800073dd52a9eef32ef929bf47b9b3c4ece5ae
jnoelg <jnoelg@users.noreply.github.com>
2015-06-19 21:03:18 -0400

    Refactoring + Center Align

--- src/main.c
+++ src/main.c
@@ -2,47 +2,99 @@
 
 static Window *s_main_window;
 
+// graphical elements
 static BitmapLayer *s_h1_img_layer;
 static BitmapLayer *s_h2_img_layer;
 static BitmapLayer *s_m1_img_layer;
 static BitmapLayer *s_m2_img_layer;
-
-static TextLayer *s_time_layer_dte;
-
-static BitmapLayer *s_warning_img_layer;
-
 static GBitmap *s_h1_bitmap;
 static GBitmap *s_h2_bitmap;
 static GBitmap *s_m1_bitmap;
 static GBitmap *s_m2_bitmap;
+
+static BitmapLayer *s_warning_img_layer;
 static GBitmap *s_warning_bitmap;
 
-static bool lastBtStateConnected = false;
+static TextLayer *s_time_layer_dte;
 
 static Layer *s_canvas_layer;
 
-static int chargeState = -1;
-
 static GFont s_time_font_dte;
 
+// states
+static bool lastBtStateConnected = false;
+static int chargeState = -1;
+
+// time decomposition
 static int h1 = 0;
 static int h2 = 0;
 static int m1 = 0;
 static int m2 = 0;
 
+// images collections
+const int IMAGE_HOUR_RESOURCE_IDS[10] = {
+  RESOURCE_ID_ZERO_BOLD, RESOURCE_ID_ONE_BOLD, RESOURCE_ID_TWO_BOLD,
+  RESOURCE_ID_THREE_BOLD, RESOURCE_ID_FOUR_BOLD, RESOURCE_ID_FIVE_BOLD,
+  RESOURCE_ID_SIX_BOLD, RESOURCE_ID_SEVEN_BOLD, RESOURCE_ID_EIGHT_BOLD,
+  RESOURCE_ID_NINE_BOLD
+}; 
+
+const int IMAGE_MIN_RESOURCE_IDS[10] = {
+  RESOURCE_ID_ZERO, RESOURCE_ID_ONE, RESOURCE_ID_TWO,
+  RESOURCE_ID_THREE, RESOURCE_ID_FOUR, RESOURCE_ID_FIVE,
+  RESOURCE_ID_SIX, RESOURCE_ID_SEVEN, RESOURCE_ID_EIGHT,
+  RESOURCE_ID_NINE
+}; 
+
+// safe getters
+static int get_image_hour(int idx) {
+  if ((idx >= 0) && (idx <= 9)) 
+    return IMAGE_HOUR_RESOURCE_IDS[idx];
+  else
+    return RESOURCE_ID_BLANK;
+}
+
+static int get_image_min(int idx) {
+  if ((idx >= 0) && (idx <= 9)) 
+    return IMAGE_MIN_RESOURCE_IDS[idx];
+  else
+    return RESOURCE_ID_BLANK;
+}
+
+// widths
 const int WIDTHS[10] = { 35, 15, 33, 30, 33, 32, 33, 33, 33, 33};
 
-/*static int get_total_width() {
+// safe width getter
+static int get_width(int idx) {
+  if ((idx >= 0) && (idx <= 9)) 
+    return WIDTHS[idx];
+  else
+    return 0;
+}
+
+// calc total width
+static int get_total_width() {
   int res = 0;
   
-  res += WIDTHS[h1];
-  res += WIDTHS[h2];
-  res += WIDTHS[m1];
-  res += WIDTHS[m2];
+  res += get_width(h1);
+  res += get_width(h2);
+  res += get_width(m1);
+  res += get_width(m2);
   
   return res;
-}*/
+}
 
+// convert ascii digit to its value
+static int ascii_digit_to_int(int ascii_val) {
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "ascii_digit_to_int: ascii_val = %d", ascii_val);
+  // ASCII 48 = '0' ... 57 = '9'
+  if ((ascii_val >= 48) && (ascii_val <= 57)) 
+    return (ascii_val - 48);
+  else 
+    return -1;
+}
+
+// color bar drawing
 static void layer_update_callback(Layer *me, GContext *ctx) {
   //APP_LOG(APP_LOG_LEVEL_DEBUG, "Getting chargeState = %d", chargeState);
   
@@ -71,6 +123,78 @@ static void layer_update_callback(Layer *me, GContext *ctx) {
   graphics_draw_line(ctx, GPoint(24,91), GPoint(120, 91));
 }
 
+static void unload_time_images() {
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "unload_time_images");
+  
+  // h1
+  layer_remove_from_parent(bitmap_layer_get_layer(s_h1_img_layer));
+  bitmap_layer_destroy(s_h1_img_layer);
+  gbitmap_destroy(s_h1_bitmap);
+  
+  // h2
+  layer_remove_from_parent(bitmap_layer_get_layer(s_h2_img_layer));
+  bitmap_layer_destroy(s_h2_img_layer);
+  gbitmap_destroy(s_h2_bitmap);
+  
+  // m1
+  layer_remove_from_parent(bitmap_layer_get_layer(s_m1_img_layer));
+  bitmap_layer_destroy(s_m1_img_layer);
+  gbitmap_destroy(s_m1_bitmap);
+  
+  // h1
+  layer_remove_from_parent(bitmap_layer_get_layer(s_m2_img_layer));
+  bitmap_layer_destroy(s_m2_img_layer);
+  gbitmap_destroy(s_m2_bitmap);
+}
+
+static void load_time_images() {
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "load_time_images");
+  
+  Layer *window_layer = window_get_root_layer(s_main_window);
+  
+  // center align
+  int total_w = get_total_width();
+  int current_x = (144 - total_w) / 2;
+  
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "total_w = %d", total_w);
+  
+  // Create and add the H1 Bitmap Layer
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "current_x = %d, h1 = %d", current_x, h1);
+  s_h1_img_layer = bitmap_layer_create(GRect(current_x, 27, get_width(h1), 43));
+  //layer_set_bounds(bitmap_layer_get_layer(s_h1_img_layer), GRect(current_x, 27, get_width(h1), 43));
+  s_h1_bitmap = gbitmap_create_with_resource(get_image_hour(h1));
+  bitmap_layer_set_bitmap(s_h1_img_layer, s_h1_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_h1_img_layer));
+  
+  // Create and add the H2 Bitmap Layer
+  current_x += get_width(h1);
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "current_x = %d, h2 = %d", current_x, h2);
+  s_h2_img_layer = bitmap_layer_create(GRect(current_x, 27, get_width(h2), 43));
+  //layer_set_bounds(bitmap_layer_get_layer(s_h2_img_layer), GRect(current_x, 27, get_width(h2), 43));
+  s_h2_bitmap = gbitmap_create_with_resource(get_image_hour(h2));
+  bitmap_layer_set_bitmap(s_h2_img_layer, s_h2_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_h2_img_layer));
+  
+  // Create and add the M1 Bitmap Layer
+  current_x += get_width(h2);
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "current_x = %d, m1 = %d", current_x, m1);
+  s_m1_img_layer = bitmap_layer_create(GRect(current_x, 28, get_width(m1), 42));
+  //layer_set_bounds(bitmap_layer_get_layer(s_m1_img_layer), GRect(current_x, 28, get_width(m1), 42));
+  s_m1_bitmap = gbitmap_create_with_resource(get_image_min(m1));
+  bitmap_layer_set_bitmap(s_m1_img_layer, s_m1_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_m1_img_layer));
+  
+  // Create and add the M2 Bitmap Layer
+  current_x += get_width(m1);
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "current_x = %d, m2 = %d", current_x, m2);
+  s_m2_img_layer = bitmap_layer_create(GRect(current_x, 28, get_width(m2), 42));
+  //layer_set_bounds(bitmap_layer_get_layer(s_m2_img_layer), GRect(current_x, 28, get_width(m2), 42));
+  s_m2_bitmap = gbitmap_create_with_resource(get_image_min(m2));
+  bitmap_layer_set_bitmap(s_m2_img_layer, s_m2_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_m2_img_layer));
+}
+
+// main window loading (initialisation)
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect frame = layer_get_frame(window_layer);
@@ -78,29 +202,8 @@ static void main_window_load(Window *window) {
   // Create GFonts 
   s_time_font_dte = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_AERO_28));
 
-  // Create and add the H1 Bitmap Layer
-  s_h1_img_layer = bitmap_layer_create(GRect(2, 27, 36, 43));
-  s_h1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ZERO_BOLD);
-  bitmap_layer_set_bitmap(s_h1_img_layer, s_h1_bitmap);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_h1_img_layer));
-  
-  // Create and add the H2 Bitmap Layer
-  s_h2_img_layer = bitmap_layer_create(GRect(37, 27, 36, 43));
-  s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ZERO_BOLD);
-  bitmap_layer_set_bitmap(s_h2_img_layer, s_h2_bitmap);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_h2_img_layer));
-  
-  // Create and add the M1 Bitmap Layer
-  s_m1_img_layer = bitmap_layer_create(GRect(72, 28, 36, 42));
-  s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ZERO);
-  bitmap_layer_set_bitmap(s_m1_img_layer, s_m1_bitmap);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_m1_img_layer));
-  
-  // Create and add the M2 Bitmap Layer
-  s_m2_img_layer = bitmap_layer_create(GRect(107, 28, 36, 42));
-  s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ZERO);
-  bitmap_layer_set_bitmap(s_m2_img_layer, s_m2_bitmap);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_m2_img_layer));
+  // Initial time (00:00)
+  load_time_images();
   
   // Create and add the time TextLayer DTE
   s_time_layer_dte = text_layer_create(GRect(0, 96, 144, 32));
@@ -123,31 +226,27 @@ static void main_window_load(Window *window) {
   layer_add_child(window_layer, bitmap_layer_get_layer(s_warning_img_layer));
 }
 
+// main window unloading (destruction)
 static void main_window_unload(Window *window) {
-  // Destroy TextLayers
+  // Unload font
+  fonts_unload_custom_font(s_time_font_dte);
+
+  // Destroy text layer
   text_layer_destroy(s_time_layer_dte);
   
-  // Unload GFonts
-  fonts_unload_custom_font(s_time_font_dte);
-  
-  // Destroy Layers
+  // Destroy canvas layer
   layer_destroy(s_canvas_layer);
   
-  // Destroy BitmapLayers and content
-  gbitmap_destroy(s_h1_bitmap);
-  gbitmap_destroy(s_h2_bitmap);
-  gbitmap_destroy(s_m1_bitmap);
-  gbitmap_destroy(s_m2_bitmap);
-  gbitmap_destroy(s_warning_bitmap);
-
-  bitmap_layer_destroy(s_h1_img_layer);
-  bitmap_layer_destroy(s_h2_img_layer);
-  bitmap_layer_destroy(s_m1_img_layer);
-  bitmap_layer_destroy(s_m2_img_layer);
+  // Remove and destroy bitmap layers and images
+  unload_time_images();
+  
+  layer_remove_from_parent(bitmap_layer_get_layer(s_warning_img_layer));
   bitmap_layer_destroy(s_warning_img_layer);
+  gbitmap_destroy(s_warning_bitmap);
 }
 
-static void update_time() {
+// update display
+static void update_display() {
   // Getting Battery State
   BatteryChargeState current_battery_charge_state = battery_state_service_peek();
   
@@ -177,193 +276,22 @@ static void update_time() {
     // Use 12 hour format
     strftime(buffer_hh, sizeof("00:00"), "%I", tick_time);
   }
+  
+  h1 = ascii_digit_to_int(buffer_hh[0]);
+  h2 = ascii_digit_to_int(buffer_hh[1]);
 
   // Write the current minuts into the buffer
   strftime(buffer_mm, sizeof("00"), "%M", tick_time);
   
+  m1 = ascii_digit_to_int(buffer_mm[0]);
+  m2 = ascii_digit_to_int(buffer_mm[1]);
+  
   // Write the current date into the buffer
   strftime(buffer_dte, sizeof("ddd 00 mmm"), "%a %d %b", tick_time);
   
-  // ASCII 48 = '0' ... 57 = '9'
-  
-  // Display h1
-  gbitmap_destroy(s_h1_bitmap);
-  
-  switch ( buffer_hh[0] ) {
-    case 48:
-      h1 = 0; 
-      s_h1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ZERO_BOLD);
-      break;
-    case 49:
-      h1 = 1; 
-      s_h1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ONE_BOLD);
-      break;
-    case 50:
-      h1 = 2; 
-      s_h1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TWO_BOLD);
-      break;
-    default:
-      h1 = -1; 
-      s_h1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BLANK);
-      break;
-  }
-  
-  bitmap_layer_set_bitmap(s_h1_img_layer, s_h1_bitmap);
-
-  // Display h2
-  gbitmap_destroy(s_h2_bitmap);
-  
-  switch ( buffer_hh[1] ) {
-    case 48:
-      h2 = 0; 
-      s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ZERO_BOLD);
-      break;
-    case 49:
-      h2 = 1; 
-      s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ONE_BOLD);
-      break;
-    case 50:
-      h2 = 2; 
-      s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TWO_BOLD);
-      break;
-    case 51:
-      h2 = 3; 
-      s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_THREE_BOLD);
-      break;
-    case 52:
-      h2 = 4; 
-      s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOUR_BOLD);
-      break;
-    case 53:
-      h2 = 5; 
-      s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FIVE_BOLD);
-      break;
-    case 54:
-      h2 = 6; 
-      s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SIX_BOLD);
-      break;
-    case 55:
-      h2 = 7; 
-      s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SEVEN_BOLD);
-      break;
-    case 56:
-      h2 = 8; 
-      s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_EIGHT_BOLD);
-      break;
-    case 57:
-      h2 = 9; 
-      s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_NINE_BOLD);
-      break;
-    default:
-      h2 = -1; 
-      s_h2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BLANK);
-      break;
-  }
-  
-  bitmap_layer_set_bitmap(s_h2_img_layer, s_h2_bitmap);
-  
-  // Display m1
-  gbitmap_destroy(s_m1_bitmap);
-
-  switch ( buffer_mm[0] ) {
-    case 48:
-      m1 = 0; 
-      s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ZERO);
-      break;
-    case 49:
-      m1 = 1; 
-      s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ONE);
-      break;
-    case 50:
-      m1 = 2; 
-      s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TWO);
-      break;
-    case 51:
-      m1 = 3; 
-      s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_THREE);
-      break;
-    case 52:
-      m1 = 4; 
-      s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOUR);
-      break;
-    case 53:
-      m1 = 5; 
-      s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FIVE);
-      break;
-    case 54:
-      m1 = 6; 
-      s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SIX);
-      break;
-    case 55:
-      m1 = 7; 
-      s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SEVEN);
-      break;
-    case 56:
-      m1 = 8; 
-      s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_EIGHT);
-      break;
-    case 57:
-      m1 = 9; 
-      s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_NINE);
-      break;
-    default:
-      m1 = -1; 
-      s_m1_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BLANK);
-      break;
-  }
-
-  bitmap_layer_set_bitmap(s_m1_img_layer, s_m1_bitmap);
-  
-  // Display m2
-  gbitmap_destroy(s_m2_bitmap);
-
-  switch ( buffer_mm[1] ) {
-    case 48:
-      m2 = 0; 
-      s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ZERO);
-      break;
-    case 49:
-      m2 = 1; 
-      s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ONE);
-      break;
-    case 50:
-      m2 = 2; 
-      s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TWO);
-      break;
-    case 51:
-      m2 = 3; 
-      s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_THREE);
-      break;
-    case 52:
-      m2 = 4; 
-      s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOUR);
-      break;
-    case 53:
-      m2 = 5; 
-      s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FIVE);
-      break;
-    case 54:
-      m2 = 6; 
-      s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SIX);
-      break;
-    case 55:
-      m2 = 7; 
-      s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SEVEN);
-      break;
-    case 56:
-      m2 = 8; 
-      s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_EIGHT);
-      break;
-    case 57:
-      m2 = 9; 
-      s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_NINE);
-      break;
-    default:
-      m2 = -1; 
-      s_m2_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BLANK);
-      break;
-  }
-  bitmap_layer_set_bitmap(s_m2_img_layer, s_m2_bitmap);
+  // undload and relaod time images
+  unload_time_images();
+  load_time_images();
   
   // Display values in TextLayers
   text_layer_set_text(s_time_layer_dte, buffer_dte);
@@ -387,7 +315,7 @@ static void update_time() {
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
+  update_display();
 }
 
 static void init() {
@@ -407,11 +335,11 @@ static void init() {
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
   
+  // Make sure the time is displayed from the start
+  update_display();
+  
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  
-  // Make sure the time is displayed from the start
-  update_time();
 }
 
 static void deinit() {
